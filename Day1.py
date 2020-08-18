6150961773d9c78942076e1a039ee1ef688b0fd4
dimplenagendra <msnagendramurthy@gmail.com>
2020-08-18 13:55:04 +0530

    Day-1 Solution

---
+++ Day1.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def rotate_left(a,d):
+    return a[d:] + a[:d]
+
+if __name__ == '__main__':
+    
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+    result=rotate_left(a,d)
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+ 
+    fptr.close()
